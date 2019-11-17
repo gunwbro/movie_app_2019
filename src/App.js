2c10c6c83f198002e249ad038cb2950b686e39f1
Gunwoo Jo <gunwbro@gmail.com>
2019-11-17 20:44:12 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import PropTypes from "prop-types";
-
+import axios from "axios";
 class App extends React.Component{
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   }
-  add = () =>{
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () =>{
-    this.setState(current => ({count: current.count - 1}));
-  };
+  componentDidMount(){                      //render가 되면 호출되는 함수
+
+    axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
   render(){
+    const { isLoading } = this.state;
     return  (
       <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {
+          isLoading ? "Loading" : "We are ready"
+        }
       </div>
     );
   }
