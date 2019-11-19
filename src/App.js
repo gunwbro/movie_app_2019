b97924d7d86bf56bb9544c70bcd76430c077e634
Gunwoo Jo <gunwbro@gmail.com>
2019-11-20 00:52:40 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -12,30 +13,40 @@ class App extends React.Component {
       data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false })
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
-  componentDidMount() {                      //render가 되면 호출되는 함수
+  componentDidMount() {
     this.getMovies();
-  };
+  }
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {
-          isLoading
-            ? "Loading"
-            : movies.map(movie => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
               <Movie
+                key={movie.id}
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
-                poster={movie.medium_cover_image} />
-            ))
-        }
-      </div>
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
+
 export default App;
